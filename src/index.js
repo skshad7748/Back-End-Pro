3049e3f3f6433838cd5ffc90748ddde7a7b301cf
skshad7748 <hasibul15-7748@diu.edu.bd>
2024-07-06 17:40:09 +0600

    Utils code discusion

--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ dotenv.config({
     path: './env'
 })
 connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(`server is running : ${process.env.PORT}`)
+    });
+})
+.catch((error)=> {
+    console.log("Mongo db connection fail")
+})
 
 
 
